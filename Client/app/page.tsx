530b5e5ded3aae2ba48c1ad06955c4e9b2fe3e93
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-03-27 21:29:47 +0330

    Add default colors (Dark) to tailwind

--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -2,9 +2,11 @@
 export default function Home() {
   return (
     <div>
-      <p>
-        Say My Name ...
-      </p>
+      colors test:
+        <div className="bg-Dark-first w-full h-60">Dark-first</div>
+        <div className="bg-Dark-second w-full h-60">Dark-second</div>
+        <div className="bg-Dark-third w-full h-60">Dark-third</div>
+        <div className="bg-Dark-fourth w-full h-60">Dark-fourth</div>
     </div>
   );
 }
