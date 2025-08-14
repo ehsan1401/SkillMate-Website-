6ce79fd5805a068dcdc80869eb1a37a6a3f53634
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-08-14 18:07:34 +0330

    add prisma and compelete database conection

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], //Front-End port and Address - it's Local for now
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
