b29c8fc767dd9b1f2e98c48a475aa10c28b34bfc
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-09-28 23:47:49 +0330

    add api ( userInfo Existance , Get userInfo , Update UserInfo)

--- Server/src/users/users.controller.ts
+++ Server/src/users/users.controller.ts
@@ -35,55 +35,51 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
 
-    @Post()
-    create(@Body() createUserDto: CreateUser) {
-      return this.usersService.create(createUserDto);
-    }
-
-    @Post('upload-avatar')
-    @UseGuards(JwtAuthGuard)
-    @UseInterceptors(
-      FileInterceptor('file', {
-        storage: diskStorage({
-          destination: './uploads/avatars',
-          filename: (req, file, callback) => {
-            const uniqueSuffix =
-              Date.now() + '-' + Math.round(Math.random() * 1e9);
-            callback(
-              null,
-              `${uniqueSuffix}${extname(file.originalname)}`,
-            );
-          },
-        }),
-      }),
-    )
-    async uploadAvatar(@UploadedFile() file: Express.Multer.File, @Request() req: ExpressRequest) {
-      const user = req.user as JwtUserPayload;
-      return this.usersService.updateAvatar(user.email, file.filename);
-    }
-
-
-@Get('protected')
-@UseGuards(JwtAuthGuard)
-async getProtected(@Request() req: ExpressRequest) {
-  const userJwt = req.user as { sub: number; email: string };
-  const user = await this.usersService.findByEmail(userJwt.email);
-  if (!user) {
-    throw new NotFoundException('User not found');
+  @Post()
+  create(@Body() createUserDto: CreateUser) {
+    return this.usersService.create(createUserDto);
   }
 
-  return {
-    userName: user.userName,
-    email: user.email,
-    type: user.type,
-    profileImageUrl: user.profileImageUrl,
-    lastLogin: user.lastLogin,
-    createAt: user.createAt,
-    updateAt: user.updateAt,
-  };
-}
+  @Post('upload-avatar')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/avatars',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          callback(
+            null,
+            `${uniqueSuffix}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  async uploadAvatar(@UploadedFile() file: Express.Multer.File, @Request() req: ExpressRequest) {
+    const user = req.user as JwtUserPayload;
+    return this.usersService.updateAvatar(user.email, file.filename);
+  }
 
 
+  @Get('protected')
+  @UseGuards(JwtAuthGuard)
+  async getProtected(@Request() req: ExpressRequest) {
+    const userJwt = req.user as { sub: number; email: string };
+    const user = await this.usersService.findByEmail(userJwt.email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
 
-
+    return {
+      userName: user.userName,
+      email: user.email,
+      type: user.type,
+      profileImageUrl: user.profileImageUrl,
+      lastLogin: user.lastLogin,
+      createAt: user.createAt,
+      updateAt: user.updateAt,
+    };
+  }
 }
