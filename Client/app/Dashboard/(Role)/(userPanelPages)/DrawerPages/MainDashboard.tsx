c3c8c397a0d2993da9c3b04559870252212ad8c3
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-11-14 00:17:08 +0330

    Let's Sync Section implanted!!

--- Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboard.tsx
+++ Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboard.tsx
@@ -19,15 +19,11 @@ export default function MainDashboard(){
            
 
     return(
-        <div className="p-5 w-full h-full select-none overflow-y-scroll">
+        <div className="px-5 py-5 w-full h-full select-none overflow-y-scroll">
             <div className="w-full flex flex-col lg:flex-row gap-0 h-16">
                 <h1 className="text-5xl text-neutral-950 dark:text-neutral-50 w-full h-full font-scriptMtbold"> 
                     Dashboard/ <span className="text-4xl">{panelType}</span>
                 </h1>
-                {/* <span className="lg:w-2/3 w-full lg:h-16 p-3 -mt-5 lg:mt-0 flex justify-center items-center gap-1 text-neutral-800 dark:text-neutral-100">
-                        <MaterialSymbolsNotificationsOutline className="scale-125"/>
-                        this is some notifications goes here
-                </span> */}
             </div>
             {PanelRender()}
 
--- Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboardPages/CollaboratorPanel.tsx
+++ Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboardPages/CollaboratorPanel.tsx
@@ -5,15 +5,18 @@ import { ConnectionPoint } from "@/Icons/ConnectionPoint";
 import { IonHeartCircleOutline } from "@/Icons/FavoriteCircle";
 import { MaterialSymbolsNotificationsOutline } from "@/Icons/NotificationsIcon";
 import { theRoutes } from "@/utils/theRoutes";
-import { Avatar, Badge } from "antd";
+import { Avatar, Badge, Button } from "antd";
 import Link from "next/link";
 import { lazy, ReactNode, Suspense } from "react";
 import LoadingSpinner from "@/Components/Loadings/LoadingSpinner";
 import EmptyFolder from "@/Icons/Vector/EmptyFolder";
 import dynamic from "next/dynamic";
 import { BouncedDots } from "@/Components/Loadings/BouncedDots";
-import { SwiperSlide , Swiper } from "swiper/react";
+import { SwiperSlide , Swiper, useSwiper } from "swiper/react";
 import { FreeMode, Navigation, Pagination } from "swiper/modules";
+import { RightSideArrow } from "@/Icons/RightSideArrow";
+import { StreamlineDelete1Solid } from "@/Icons/RemoveIcon";
+import { TrashBin } from "@/Icons/TrashBin";
 
 const ProfileCompletion = dynamic(()=>import('./pages/ProfileCompletion') , {
     loading : ()=> <div className="w-full h-full bg-gray-300 animate-pulse flex justify-center items-center"><BouncedDots/></div>
@@ -36,41 +39,42 @@ type Suggeste = {
 export default function CollaboratorPanel(){
     const { userInfo } = useUser();      
     const {TogglePanelItem} = useChangePanelItem();
+    const swiper = useSwiper();
 
     const Suggestes : Suggeste[] = [
         {    
             profileURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&s",
             name : "John Wick" , 
             skills : [
-                "NestJs" , "Front-end" , "TailwindCss" , "Ant Design"
+                "NestJs" , "Front-end" 
             ]
         },
         {    
             profileURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOZY7Pr9nMm5Ev1ykgFkD6A1u6p7aiZfpuZ5dUU5j3bENgLbtqIjNAiY2JG9GiBWvVGY&usqp=CAU",
             name : "Billy Forger" , 
             skills : [
-                "NestJs" , "Front-end" , "TailwindCss" , "Ant Design"
+                "NestJs" , "Ant Design"
             ]
         },
         {    
             profileURL: "https://media.licdn.com/dms/image/v2/D4E03AQEyMNcR8Zvk9g/profile-displayphoto-shrink_400_400/B4EZQckdREHgAg-/0/1735646113112?e=2147483647&v=beta&t=_ATONMcbGg3Z0p_lZPAwNWCktLJ7j1CvBr5G2dakr6I",
             name : "Random Girl" , 
             skills : [
-                "NestJs" , "Front-end" , "TailwindCss" , "Ant Design"
+                "TailwindCss" , "Ant Design"
             ]
         },
         {    
             profileURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe3ngiKS0US40aYzurZP62I20phqRbO3SEtxCvrVvfV6SzH37jWG_rvgZwJKJ10u3zi78&usqp=CAU",
             name : "Random Boy" , 
             skills : [
-                "NestJs" , "Front-end" , "TailwindCss" , "Ant Design"
+                "Front-end" , "TailwindCss"
             ]
         },
         {    
             profileURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe3ngiKS0US40aYzurZP62I20phqRbO3SEtxCvrVvfV6SzH37jWG_rvgZwJKJ10u3zi78&usqp=CAU",
             name : "Random Boy" , 
             skills : [
-                "NestJs" , "Front-end" , "TailwindCss" , "Ant Design"
+                "TailwindCss" , "Ant Design"
             ]
         },
 
@@ -114,9 +118,9 @@ export default function CollaboratorPanel(){
     ]
     
     return(
-        <div className="w-full lg:h-[86%] h-[80%] mt-8 lg:mt-0 flex md:flex-row flex-col-reverse">
-            <div className=" md:w-3/5 w-full h-full -mr-[20px]">
-                <div className=" w-full h-3/5 pt-10 pb-8 flex gap-2 ">
+        <div className="w-full lg:h-[87%] h-[80%] mt-8 lg:mt-0 flex md:flex-row flex-col-reverse">
+            <div className=" md:w-3/5 w-full h-[102%] -mr-[20px]">
+                <div className=" w-full h-3/5 pt-5 pb-12 flex gap-2 ">
                     <div className="w-1/2 h-full border-[3px] border-solid border-neutral-600 dark:border-neutral-100 rounded-md overflow-hidden">
                         <ProfileCompletion/>
                     </div>
@@ -128,61 +132,75 @@ export default function CollaboratorPanel(){
                         </div>
                     </div>
                 </div>
-                <div className="w-full h-2/5 border-[3px] border-solid border-neutral-600 dark:border-neutral-100 rounded-md -mt-4 px-3 py-2">
+                <div className="w-full h-[43%] border-[3px] border-solid border-neutral-600 dark:border-neutral-100 rounded-md -mt-8 px-3 py-2">
                 <div className=" w-full relative">
-                    <h2 className="text-neutral-800 dark:text-neutral-100 font-vazir text-lg">Let&apos;s Sync!</h2>
-                    <button className="absolute w-auto h-full right-1 top-0 text-sm px-2 font-Lalezar flex flex-col hover:underline">
+                    <h2 className="text-neutral-800 dark:text-neutral-100 font-vazir text-lg pl-2 ">Let&apos;s Sync!</h2>
+                    <button className="absolute w-auto h-full right-1 top-1 text-sm px-2 font-Lalezar flex flex-col hover:underline">
                         All Suggestions
                     </button>
                 </div>
-                <div className="w-full h-[75%] bg-red-800 p-3">
-                <Swiper
-                    pagination={{
+                <div className="w-full h-[75%] px-3 flex items-center justify-center relative">
+                    <Swiper
+                        navigation={{ nextEl: '.next-btn', prevEl: '.prev-btn' }}
+                        modules={[FreeMode , Navigation]}
+                        spaceBetween={8}
+                        slidesPerView={3}
+                        direction="horizontal"
+                        loop={true}
+                        pagination={{
                         clickable: true,
-                    }}
-                    modules={[FreeMode, Pagination]}
-                    spaceBetween={5}
-                    slidesPerView={4}
-                    navigation
-                    direction="horizontal"
+                        }}
+                        
+                        className="[&_.swiper-button-next]:!w-6 [&_.swiper-button-next]:!h-6 [&_.swiper-button-next:after]:!text-[16px] [&_.swiper-button-prev]:!w-6 [&_.swiper-button-prev]:!h-6 [&_.swiper-button-prev:after]:!text-[16px]"
 
-                >
-                    {Suggestes.map((person: Suggeste) => {
-                        return(
-                            <SwiperSlide key={person.name}>
-                                <div className="flex flex-col items-center p-3 bg-white rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300">
-                                {/* <img
-                                    src={person.profileURL}
-                                    alt={person.name}
-                                    className="w-20 h-20 rounded-full object-cover mb-2"
-                                /> */}
-                                <Avatar
-                                    size={40}
-                                    src={person.profileURL}
-                                    className="z-10 mx-[0px] border-2 border-solid border-neutral-600 shadow-md"
-                                />
-                                <span className="font-semibold text-sm">{person.name}</span>
-                                <div className="flex flex-wrap justify-center gap-1 mt-1">
-                                    {person.skills.map((skill) => (
-                                    <span
-                                        key={skill}
-                                        className="px-2 py-1 text-xs bg-gray-200 dark:bg-gray-700 rounded-full"
-                                    >
-                                        {skill}
-                                    </span>
-                                    ))}
-                                </div>
-                                </div>
-                            </SwiperSlide>
-                        )
-                    } )}
-                </Swiper>
+                    >
+                        {Suggestes.map((person: Suggeste) => {
+                            return(
+                                <SwiperSlide key={person.name}>
+                                    <div className="flex flex-col items-center justify-center p-3 bg-neutral-300 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 h-full relative">
+                                        <Avatar
+                                            size={40}
+                                            src={person.profileURL}
+                                            className="z-10 mx-[0px] border-2 border-solid border-neutral-600 shadow-md"
+                                        />
+                                        <span className="font-vazir font-bold text-base pt-1">{person.name}</span>
+                                        <div className="flex flex-wrap justify-center gap-1 py-1">
+                                            {person.skills.map((skill) => (
+                                            <span
+                                                key={skill}
+                                                className="text-xs rounded-full px-[2px]"
+                                            >
+                                                {skill}
+                                            </span>
+                                            ))}
+                                        </div>
+                                        <Button type="primary" size="small" className="px-14 font-Homa">Sync</Button>
+                                    </div>
+                                    <button className="absolute top-3 right-4 group w-6 h-6">
+                                        <StreamlineDelete1Solid
+                                            className="text-xs absolute inset-0 transition-opacity duration-300 group-hover:opacity-0 ml-[5px] mt-[4px]"
+                                        />
+                                        <TrashBin
+                                            className="text-xl absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-red-600"
+                                        />
+                                    </button>
+
+
+                                </SwiperSlide>
+                            )
+                        } )}
+                    </Swiper>
+                    <button 
+                        className=" next-btn absolute right-2 top-12 z-10 text-black text-2xl hover:text-blue-500 hover:scale-110 transition-all duration-300" 
+                    >
+                        <RightSideArrow />
+                    </button>
                 </div>
 
 
                 </div>
             </div>
-            <aside className="md:w-2/5 w-full px-5 h-full text-center py-3 flex md:flex-col flex-row md:items-center justify-center md:gap-3 gap-5  pt-8">
+            <aside className="md:w-2/5 w-full px-5 h-[102%] text-center py-[5px] flex md:flex-col flex-row md:items-center justify-center md:gap-2 gap-0 pt-[17px]">
                 <div className="w-full h-3/6 grid grid-cols-3">
                 
                 {
@@ -201,7 +219,7 @@ export default function CollaboratorPanel(){
                 }
 
                 </div>
-                <div className=" w-full h-3/6 px-[15px] py-1">
+                <div className=" w-full h-[50%] px-[15px] pt-1 pb-4">
                     <div className="border-[3px] border-solid border-neutral-600 dark:border-neutral-200 h-full rounded-md">
 
                     </div>
