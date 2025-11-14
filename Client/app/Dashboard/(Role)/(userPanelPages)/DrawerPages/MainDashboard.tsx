ac1f26a673a8aad17451f4560d537b1c43baa68f
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-11-14 16:13:28 +0330

    Collaborator Panel Design Completed!

--- Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboard.tsx
+++ Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboard.tsx
@@ -19,7 +19,7 @@ export default function MainDashboard(){
            
 
     return(
-        <div className="px-5 py-5 w-full h-full select-none overflow-y-scroll">
+        <div className="px-5 py-5 w-full h-full select-none scrollbar-none">
             <div className="w-full flex flex-col lg:flex-row gap-0 h-16">
                 <h1 className="text-5xl text-neutral-950 dark:text-neutral-50 w-full h-full font-scriptMtbold"> 
                     Dashboard/ <span className="text-4xl">{panelType}</span>
--- Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboardPages/CollaboratorPanel.tsx
+++ Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/MainDashboardPages/CollaboratorPanel.tsx
@@ -1,14 +1,12 @@
 import { useChangePanelItem } from "@/Components/context/PanelItem/PanelItemsProvider";
 import { useUser } from "@/Components/context/UserContext/UserContext";
-import { AlternateEmailRounded } from "@/Icons/AlternateEmailRounded";
 import { ConnectionPoint } from "@/Icons/ConnectionPoint";
 import { IonHeartCircleOutline } from "@/Icons/FavoriteCircle";
 import { MaterialSymbolsNotificationsOutline } from "@/Icons/NotificationsIcon";
 import { theRoutes } from "@/utils/theRoutes";
 import { Avatar, Badge, Button } from "antd";
 import Link from "next/link";
-import { lazy, ReactNode, Suspense } from "react";
-import LoadingSpinner from "@/Components/Loadings/LoadingSpinner";
+import { ReactNode } from "react";
 import EmptyFolder from "@/Icons/Vector/EmptyFolder";
 import dynamic from "next/dynamic";
 import { BouncedDots } from "@/Components/Loadings/BouncedDots";
@@ -17,6 +15,12 @@ import { FreeMode, Navigation, Pagination } from "swiper/modules";
 import { RightSideArrow } from "@/Icons/RightSideArrow";
 import { StreamlineDelete1Solid } from "@/Icons/RemoveIcon";
 import { TrashBin } from "@/Icons/TrashBin";
+import { GraphNewBroken } from "@/Icons/GraphNewBroken";
+import { RequestIcon } from "@/Icons/RequestIcon";
+import { MdiChat } from "@/Icons/ChatIcon";
+import { FileCopy } from "@/Icons/FileCopy";
+import { TeamIcon } from "@/Icons/TeamIcon";
+import Image from "next/image";
 
 const ProfileCompletion = dynamic(()=>import('./pages/ProfileCompletion') , {
     loading : ()=> <div className="w-full h-full bg-gray-300 animate-pulse flex justify-center items-center"><BouncedDots/></div>
@@ -82,10 +86,10 @@ export default function CollaboratorPanel(){
     
     const boxItems : boxItem[] = [
         {
-            name : 'Favorite' ,
-            route : theRoutes.Dashboard.favorite,
-            Icon : <IonHeartCircleOutline className="text-4xl"/> ,
-            count : userInfo?.favorite.People.length
+            name : 'Messages',
+            route : '#',
+            Icon : <MdiChat className="text-3xl mb-1"/>,
+            count : 0
         },
         {
             name : 'Notifications' ,
@@ -97,28 +101,42 @@ export default function CollaboratorPanel(){
         {
             name : 'Connections' ,
             route : '#',
-            Icon : <ConnectionPoint className="text-4xl"/> ,
+            Icon : <ConnectionPoint className="text-3xl mb-1"/> ,
             count : 0
         },
         {
-            name : 'something',
+            name : 'Profile Analyse',
             route : '#',
-            Icon : <AlternateEmailRounded/>
+            Icon : <GraphNewBroken className="text-3xl mb-1"/>,
+            count : 0
         },
         {
-            name : 'asd',
+            name : 'Favorite' ,
+            route : theRoutes.Dashboard.favorite,
+            Icon : <IonHeartCircleOutline className="text-4xl"/> ,
+            count : userInfo?.favorite.People.length
+        },
+        
+        {
+            name : 'Requests',
             route : '#',
-            Icon : <AlternateEmailRounded/>
+            Icon : <RequestIcon  className="text-4xl"/>,
+            count : 0
         },
         {
-            name : 'asdadadad',
+            name : 'Projects',
             route : '#',
-            Icon : <AlternateEmailRounded/>
+            Icon : <FileCopy className="text-3xl mb-1"/>
+        },
+        {
+            name : 'Create Team',
+            route : '#',
+            Icon : <TeamIcon className="text-3xl mb-1"/>
         }
     ]
     
     return(
-        <div className="w-full lg:h-[87%] h-[80%] mt-8 lg:mt-0 flex md:flex-row flex-col-reverse">
+        <div className="w-full lg:h-[87%] h-[80%] mt-8 lg:mt-0 flex md:flex-row flex-col-reverse justify-center items-center ">
             <div className=" md:w-3/5 w-full h-[102%] -mr-[20px]">
                 <div className=" w-full h-3/5 pt-5 pb-12 flex gap-2 ">
                     <div className="w-1/2 h-full border-[3px] border-solid border-neutral-600 dark:border-neutral-100 rounded-md overflow-hidden">
@@ -135,7 +153,7 @@ export default function CollaboratorPanel(){
                 <div className="w-full h-[43%] border-[3px] border-solid border-neutral-600 dark:border-neutral-100 rounded-md -mt-8 px-3 py-2">
                 <div className=" w-full relative">
                     <h2 className="text-neutral-800 dark:text-neutral-100 font-vazir text-lg pl-2 ">Let&apos;s Sync!</h2>
-                    <button className="absolute w-auto h-full right-1 top-1 text-sm px-2 font-Lalezar flex flex-col hover:underline">
+                    <button className="absolute w-auto h-full right-1 top-1 text-sm px-2 font-Lalezar flex flex-col hover:underline text-neutral-800 dark:text-neutral-100">
                         All Suggestions
                     </button>
                 </div>
@@ -157,18 +175,18 @@ export default function CollaboratorPanel(){
                         {Suggestes.map((person: Suggeste) => {
                             return(
                                 <SwiperSlide key={person.name}>
-                                    <div className="flex flex-col items-center justify-center p-3 bg-neutral-300 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 h-full relative">
+                                    <div className="flex flex-col items-center justify-center p-3 bg-neutral-200 dark:bg-neutral-700 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 h-full relative">
                                         <Avatar
                                             size={40}
                                             src={person.profileURL}
                                             className="z-10 mx-[0px] border-2 border-solid border-neutral-600 shadow-md"
                                         />
-                                        <span className="font-vazir font-bold text-base pt-1">{person.name}</span>
-                                        <div className="flex flex-wrap justify-center gap-1 py-1">
+                                        <span className="font-vazir text-lg pt-2 text-neutral-800 dark:text-neutral-100">{person.name}</span>
+                                        <div className="flex flex-wrap justify-center gap-1 pb-1">
                                             {person.skills.map((skill) => (
                                             <span
                                                 key={skill}
-                                                className="text-xs rounded-full px-[2px]"
+                                                className="text-xs rounded-full px-[2px] text-neutral-800 dark:text-neutral-100 font-gothic"
                                             >
                                                 {skill}
                                             </span>
@@ -178,20 +196,18 @@ export default function CollaboratorPanel(){
                                     </div>
                                     <button className="absolute top-3 right-4 group w-6 h-6">
                                         <StreamlineDelete1Solid
-                                            className="text-xs absolute inset-0 transition-opacity duration-300 group-hover:opacity-0 ml-[5px] mt-[4px]"
+                                            className="text-xs absolute inset-0 transition-opacity duration-300 group-hover:opacity-0 ml-[5px] mt-[4px] text-neutral-800 dark:text-neutral-100"
                                         />
                                         <TrashBin
-                                            className="text-xl absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-red-600"
+                                            className="text-xl absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-red-600 dark:text-red-400"
                                         />
                                     </button>
-
-
                                 </SwiperSlide>
                             )
                         } )}
                     </Swiper>
                     <button 
-                        className=" next-btn absolute right-2 top-12 z-10 text-black text-2xl hover:text-blue-500 hover:scale-110 transition-all duration-300" 
+                        className=" next-btn absolute right-2 top-12 z-10 text-neutral-800 dark:text-neutral-100 text-2xl hover:text-blue-500 hover:dark:text-cyan-700 hover:scale-110 transition-all duration-300" 
                     >
                         <RightSideArrow />
                     </button>
@@ -200,8 +216,8 @@ export default function CollaboratorPanel(){
 
                 </div>
             </div>
-            <aside className="md:w-2/5 w-full px-5 h-[102%] text-center py-[5px] flex md:flex-col flex-row md:items-center justify-center md:gap-2 gap-0 pt-[17px]">
-                <div className="w-full h-3/6 grid grid-cols-3">
+            <aside className="md:w-2/5 w-full pl-10 h-[102%] text-center py-[5px] flex md:flex-col flex-row md:items-center justify-center md:gap-2 gap-0 pt-[17px] items-center">
+                <div className="w-full h-3/6 grid grid-cols-4 gap-2">
                 
                 {
                     boxItems.map((item : boxItem)=>{
@@ -210,7 +226,7 @@ export default function CollaboratorPanel(){
                                 <Badge count={item.count ? item.count : 0} color="orange">
                                         <div className="border-[3px] border-solid border-neutral-600 dark:border-neutral-300 w-[90px] h-[90px] justify-center items-center rounded-md hover:rounded-2xl transition-all duration-300 text-neutral-600 dark:text-neutral-100 flex flex-col gap-1 font-vazir">
                                             <span className="text-2xl flex justify-center items-center">{item.Icon}</span>
-                                            <span className="text-xs font-semibold">{item.name}</span>
+                                            <span className="text-xs font-vazir">{item.name}</span>
                                         </div>
                                 </Badge>
                             </Link>
@@ -219,8 +235,19 @@ export default function CollaboratorPanel(){
                 }
 
                 </div>
-                <div className=" w-full h-[50%] px-[15px] pt-1 pb-4">
-                    <div className="border-[3px] border-solid border-neutral-600 dark:border-neutral-200 h-full rounded-md">
+                <div className=" w-full h-[50%] px-[0px] pt-1 pb-4">
+                    <div className="border-[3px] border-solid border-neutral-600 dark:border-neutral-200 h-full rounded-md  bg-gradient-to-br from-yellow-200 to-yellow-600 flex p-5">
+
+                        <div className="w-2/3 h-full flex justify-center items-center -ml-10 -mt-2">
+                            <Image src="/Images/Become_sponsor.png" alt="Become_sponsor" width={200} height={200} />
+                        </div>
+                        <div>
+                            <p className="text-3xl text-left mt-4 mr-6 font-Franklin font-bold text-[#930223] ">
+                                BECOME <br /> SPONSOR
+                            </p>
+                            <Button variant="solid" color="magenta" className=" mr-7 -mt-9 font-vazir">Search For Projects</Button>
+                        </div>
+
 
                     </div>
                 </div>
