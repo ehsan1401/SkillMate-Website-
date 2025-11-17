3e6b5de7a5c6818edcf557ba1aa52e2387f3c6ab
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-11-17 17:36:15 +0330

    Create ChangeSeenStatus and  Allnotifications APIs

--- Server/Queries/notifications_table_queries.sql
+++ Server/Queries/notifications_table_queries.sql
@@ -27,9 +27,29 @@ BEFORE UPDATE ON notifications
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
 
--- INSERT INTO notifications (sender, receiver, type, is_none_reply, is_removed, create_at , update_at , message , replay)
--- VALUES
--- (7, 11, 'Normal', TRUE, FALSE, NOW() , NOW() ,  'something asdsada ad ad ad aad' , NULL);
+
+
+ALTER TABLE notifications ADD COLUMN is_Seen BOOLEAN DEFAULT false 
+
+
+
+
+
+INSERT INTO notifications (
+    sender, receiver, type, is_none_reply, is_removed, is_Seen, create_at, update_at, message, replay
+)
+VALUES
+(7, 11, 'Normal', true, false, false, NOW(), NOW(), 'Test notification 1', ''),
+(11, 7, 'Super', true, false, false, NOW(), NOW(), 'Test notification 2', ''),
+(10000, 11, 'System', true, false, false, NOW(), NOW(), 'Test notification 3', ''),
+(7, 10000, 'Normal', true, false, false, NOW(), NOW(), 'Test notification 4', ''),
+(11, 10000, 'Super', true, false, false, NOW(), NOW(), 'Test notification 5', ''),
+(3, 5, 'Normal', true, false, false, NOW(), NOW(), 'Test notification 6', ''),
+(12, 2, 'System', true, false, false, NOW(), NOW(), 'Test notification 7', ''),
+(3, 7, 'Super', true, false, false, NOW(), NOW(), 'Test notification 8', ''),
+(11, 12, 'Normal', true, false, false, NOW(), NOW(), 'Test notification 9', ''),
+(7, 5, 'System', true, false, false, NOW(), NOW(), 'Test notification 10', '');
+
 
 
 
--- Server/src/notifications/notifications.service.ts
+++ Server/src/notifications/notifications.service.ts
@@ -1,6 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
 
 
 @Injectable()
 export class NotificationsService {
+    constructor(private readonly databaseService : DatabaseService){}
+
+    async Allnotifications(id : number){
+        const pool = this.databaseService.getPool();
+        // const Notifications = []
+        const checkUserExist = await pool.query(`SELECT * FROM users WHERE id=$1` , [id])
+        if(checkUserExist.rowCount === 0 ) throw new BadRequestException('User dose not exist!')
+
+        const Notifications = await pool.query(
+            `SELECT notifications.type ,
+            notifications.is_none_reply,
+            notifications.is_removed,
+            notifications.is_seen,
+            notifications.message,
+            notifications.update_at,
+            notifications.create_at,
+            users."userName",
+            users."profileImageUrl",
+            users."id" AS "UserID"
+            FROM notifications INNER JOIN users ON notifications.sender = users.id
+            WHERE 
+            notifications.receiver = $1` , [id])
+
+        return Notifications.rows
+        
+        
+        // const User = await pool.query(`SELECT * FROM notifications WHERE receiver=$1` , [id])
+        // return{Data : User.rows}
+    }
+
+    async ChangeSeenStatus(NotifId : number){
+        const pool = this.databaseService.getPool();
+        const checkNotifExist = await pool.query(`SELECT * FROM notifications WHERE notif_id=$1` , [NotifId])
+        if(checkNotifExist.rowCount === 0 ) throw new BadRequestException('This Notification Dosent Exist!')
+
+        const Result = await pool.query(`UPDATE notifications SET is_seen = true WHERE notif_id=$1` , [NotifId])
+        if(Result.rowCount !== 0) return {status: 200 , message : "notification has Read!"}
+        return {status : 500 , message : "there is an Error!"}
+    }
 }
