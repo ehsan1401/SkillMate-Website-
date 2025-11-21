d5f45df0a0710b42925d84f841a8ad67786c02a9
Ehsan Jangjo <ehsan.good1382@gmail.com>
2025-11-21 18:35:52 +0330

    Create select Notification type buttons

--- Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/Notifications.tsx
+++ Client/app/Dashboard/(Role)/(userPanelPages)/DrawerPages/Notifications.tsx
@@ -1,16 +1,48 @@
-"use client";
+'use client';
 
-import DevelopingPage from "@/Components/Developing";
+import { useEffect, useState } from "react";
 
 
 export default function Notifications() {
+  const [notificationFilter , setNotificationFilter] = useState<string | undefined>("All-Notifications")
 
+  useEffect(() => {
+    const stored = localStorage.getItem("NotificationsFilter");
+    if (stored) setNotificationFilter(stored);
+  }, []);
+
+  const buttons = [
+    { name: "All Notifications", value: "All-Notifications" },
+    { name: "Unread Only", value: "Unread-Only" },
+    { name: "Important", value: "Important" },
+    { name: "System Alerts", value: "System-Alerts" },
+  ];
+  const HandleFilterNotifications = (Filter : string)=>{
+    setNotificationFilter(Filter);
+    localStorage.setItem("NotificationsFilter" , Filter)
+  }
   return (
-    <div className="p-5 w-full h-full select-none"> 
-        <h1 
-            className="text-5xl text-neutral-950 dark:text-neutral-50 font-scriptMtbold"> 
-            Notifications 
+    <div className="p-5 w-full h-full select-none">
+      <header className="h-16 flex">
+        <h1 className="text-5xl text-neutral-950 dark:text-neutral-50 font-scriptMtbold w-1/3 h-full">
+          Notifications
         </h1>
+        <section className="w-2/3 h-full flex justify-center items-center gap-3">
+          {buttons.map((item) => (
+            <button
+              key={item.value}
+              className={`px-4 py-2 border-[3px] border-solid border-neutral-600 hover:rounded-xl transition-all duration-500 ${notificationFilter === item.value ? `bg-neutral-300 rounded-xl`: ``}`}
+              onClick={()=>{HandleFilterNotifications(item.value)}}
+            >
+              {item.name}
+            </button>
+          ))}
+        </section>
+      </header>
+      <div>
+          {notificationFilter}
+          {/* <MapNotifications/> */}
+      </div>
     </div>
   );
 }
